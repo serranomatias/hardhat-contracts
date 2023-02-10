5a880e0afda8322855ecc02fa6fd3e87552ef002
Matiserrano99 <matiasserrano99@gmail.com>
2023-02-10 10:09:57 -0300

    adding files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Crypto Devs NFT Collection
+
+Crypto Devs Contract Address:  0x1Ef9469e9A5378f471aaf20AA572e43F5835AC52
---
+++ constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xaeb1fcd18ad69481f41fF2e2bA9B0801f5ECC1a0";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://cryptodev-dapp.vercel.app/api/";
+
+module.exports = {WHITELIST_CONTRACT_ADDRESS, METADATA_URL};
