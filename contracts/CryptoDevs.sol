5d5f5b29acccf06413071d4b408c4695ed647961
Matiserrano99 <matiasserrano99@gmail.com>
2023-03-01 15:06:18 -0300

    solve bug error ether sent

--- contracts/CryptoDevs.sol
+++ contracts/CryptoDevs.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
@@ -8,9 +7,9 @@ import "./IWhitelist.sol";
 
 contract CryptoDevs is ERC721Enumerable, Ownable {
     /**
-     * @dev _baseTokenURI for computing {tokenURI}. If set, the resulting URI for each
-     * token will be the concatenation of the `baseURI` and the `tokenId`.
-     */
+      * @dev _baseTokenURI for computing {tokenURI}. If set, the resulting URI for each
+      * token will be the concatenation of the `baseURI` and the `tokenId`.
+      */
     string _baseTokenURI;
 
     //  _price is the price of one Crypto Dev NFT
@@ -25,7 +24,7 @@ contract CryptoDevs is ERC721Enumerable, Ownable {
     // total number of tokenIds minted
     uint256 public tokenIds;
 
-    // whitelist contract instance
+    // Whitelist contract instance
     IWhitelist whitelist;
 
     // boolean to keep track of whether presale started or not
@@ -34,30 +33,25 @@ contract CryptoDevs is ERC721Enumerable, Ownable {
     // timestamp for when presale would end
     uint256 public presaleEnded;
 
-    modifier OnlyWhenNotPaused() {
+    modifier onlyWhenNotPaused {
         require(!_paused, "Contract currently paused");
         _;
     }
 
     /**
-     * @dev ERC721 constructor takes in a `name` and a `symbol` to the token collection.
-     * name in our case is `Crypto Devs` and symbol is `CD`.
-     * Constructor for Crypto Devs takes in the baseURI to set _baseTokenURI for the collection.
-     * It also initializes an instance of whitelist interface.
-     */
-
-    constructor(
-        string memory baseURI,
-        address whitelistContract
-    ) ERC721("Crypto Devs", "CD") {
+      * @dev ERC721 constructor takes in a `name` and a `symbol` to the token collection.
+      * name in our case is `Crypto Devs` and symbol is `CD`.
+      * Constructor for Crypto Devs takes in the baseURI to set _baseTokenURI for the collection.
+      * It also initializes an instance of whitelist interface.
+      */
+    constructor (string memory baseURI, address whitelistContract) ERC721("Crypto Devs", "CD") {
         _baseTokenURI = baseURI;
         whitelist = IWhitelist(whitelistContract);
     }
 
     /**
-     * @dev startPresale starts a presale for the whitelisted addresses
-     */
-
+    * @dev startPresale starts a presale for the whitelisted addresses
+      */
     function startPresale() public onlyOwner {
         presaleStarted = true;
         // Set presaleEnded time as current timestamp + 5 minutes
@@ -66,17 +60,11 @@ contract CryptoDevs is ERC721Enumerable, Ownable {
     }
 
     /**
-     * @dev presaleMint allows a user to mint one NFT per transaction during the presale.
-     */
-    function presaleMint() public payable OnlyWhenNotPaused {
-        require(
-            presaleStarted && block.timestamp < presaleEnded,
-            "Presale is not running"
-        );
-        require(
-            whitelist.whitelistedAddresses(msg.sender),
-            "You are not whitelisted"
-        );
+      * @dev presaleMint allows a user to mint one NFT per transaction during the presale.
+      */
+    function presaleMint() public payable onlyWhenNotPaused {
+        require(presaleStarted && block.timestamp < presaleEnded, "Presale is not running");
+        require(whitelist.whitelistedAddresses(msg.sender), "You are not whitelisted");
         require(tokenIds < maxTokenIds, "Exceeded maximum Crypto Devs supply");
         require(msg.value >= _price, "Ether sent is not correct");
         tokenIds += 1;
@@ -87,30 +75,27 @@ contract CryptoDevs is ERC721Enumerable, Ownable {
     }
 
     /**
-     * @dev maint allows  user to mint 1 NFT per transaction after the presale has ended.
-     */
-    function mint() public payable OnlyWhenNotPaused {
-        require(
-            presaleStarted && block.timestamp >= presaleEnded,
-            "Presale has not ended yet"
-        );
-        require(tokenIds < maxTokenIds, "Exceeded maximum Crypto Devs supply");
-        require(msg.value > _price, "Ether sent is not correct");
+    * @dev mint allows a user to mint 1 NFT per transaction after the presale has ended.
+    */
+    function mint() public payable onlyWhenNotPaused {
+        require(presaleStarted && block.timestamp >=  presaleEnded, "Presale has not ended yet");
+        require(tokenIds < maxTokenIds, "Exceed maximum Crypto Devs supply");
+        require(msg.value >= _price, "Ether sent is not correct");
         tokenIds += 1;
         _safeMint(msg.sender, tokenIds);
     }
 
     /**
-     * @dev _baseURI overides the Openzeppelin's ERC721 implementation which by default
-     * returned an empty string for the baseURI
-     */
+    * @dev _baseURI overides the Openzeppelin's ERC721 implementation which by default
+    * returned an empty string for the baseURI
+    */
     function _baseURI() internal view virtual override returns (string memory) {
         return _baseTokenURI;
     }
 
     /**
-     * @dev setPaused makes the contract paused or unpaused
-     */
+    * @dev setPaused makes the contract paused or unpaused
+      */
     function setPaused(bool val) public onlyOwner {
         _paused = val;
     }
@@ -119,25 +104,16 @@ contract CryptoDevs is ERC721Enumerable, Ownable {
     * @dev withdraw sends all the ether in the contract
     * to the owner of the contract
       */
-     function withdraw() public onlyOwner {
+    function withdraw() public onlyOwner  {
         address _owner = owner();
         uint256 amount = address(this).balance;
-        (bool sent, ) = _owner.call{value: amount}("");
+        (bool sent, ) =  _owner.call{value: amount}("");
         require(sent, "Failed to send Ether");
-     }
+    }
 
-     // Function to receive Ether. msg,data must be empty
-     receive() external payable {}
-     
-     // Fallback function is called when msg.data is not empty
-     fallback() external payable {}
-     
- 
+      // Function to receive Ether. msg.data must be empty
+    receive() external payable {}
 
-
-
-
-
-
-
+    // Fallback function is called when msg.data is not empty
+    fallback() external payable {}
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,12 +1,8 @@
-
-
-const { ethers } = require('hardhat');
-require('dotenv').config({ path: ".env" });
+const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
 const { WHITELIST_CONTRACT_ADDRESS, METADATA_URL } = require("../constants");
 
-
-async function main(){
-
+async function main() {
   // Address of the whitelist contract that you deployed in the previous module
   const whitelistContract = WHITELIST_CONTRACT_ADDRESS;
   // URL from where we can extract the metadata for a Crypto Dev NFT
@@ -15,28 +11,28 @@ async function main(){
   A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
   so cryptoDevsContract here is a factory for instances of our CryptoDevs contract.
   */
- const cryptoDevsContract = await ethers.getContractFactory("CryptoDevs");
+  const cryptoDevsContract = await ethers.getContractFactory("CryptoDevs");
 
- // Deploy the contract
- const deployedCryptoDevsContract = await cryptoDevsContract.deploy(
-  metadataURL,
-  whitelistContract
- );
+  // deploy the contract
+  const deployedCryptoDevsContract = await cryptoDevsContract.deploy(
+    metadataURL,
+    whitelistContract
+  );
 
- // Wait for it to finish deploying
- await deployedCryptoDevsContract.deployed();
+  // Wait for it to finish deploying
+  await deployedCryptoDevsContract.deployed();
 
- // Print the address of the deployed contract
- console.log(
-  "Crypto Devs Contract Address: ",
-  deployedCryptoDevsContract.address
+  // print the address of the deployed contract
+  console.log(
+    "Crypto Devs Contract Address:",
+    deployedCryptoDevsContract.address
   );
 }
 
 // Call the main function and catch if there is any error
 main()
-  .then(()=> process.exit(0))
-  .catch((error)=> {
+  .then(() => process.exit(0))
+  .catch((error) => {
     console.error(error);
     process.exit(1);
-  })
+  });
